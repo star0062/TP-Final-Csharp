37ebef66dbb94600f4e6fdfff0287d1e043d3561
star <najmeldeen.adamabkar@ynov.com>
2024-12-12 16:10:38 +0100

    GestionParc.cs

---
+++ GestionParc.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+
+namespace TP_Final_Csharp
+{
+    public class GestionParc
+    {
+        private List<Voiture> voitures;
+
+        public GestionParc()
+        {
+            voitures = new List<Voiture>();
+        }
+
+        public void AjouterVoiture(Voiture voiture)
+        {
+            voitures.Add(voiture);
+            Console.WriteLine("Voiture ajoutée avec succès !");
+        }
+
+        public void ListerVoitures()
+        {
+            if (voitures.Count == 0)
+            {
+                Console.WriteLine("Aucune voiture dans le parc.");
+                return;
+            }
+
+            Console.WriteLine("Liste des voitures :");
+            for (int i = 0; i < voitures.Count; i++)
+            {
+                Console.Write($"{i + 1}. ");
+                voitures[i].AfficherInfos();
+            }
+        }
+
+}
